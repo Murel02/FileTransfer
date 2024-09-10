ea821de942083c7077b1d7ededdc99ffbdbf9ab3
Murel02 <olimurel@gmail.com>
2024-09-10 20:03:52 +0200

    g

--- src/TcpClient.java
+++ src/TcpClient.java
@@ -1,2 +1,39 @@
+import java.io.*;
+import java.net.Socket;
+
 public class TcpClient {
+    public static void main(String[] args){
+        String hostname = "localhost";
+        int port = 5000;
+
+        //setting up at host port there fill output get send out if error catch Exception
+        try(Socket socket = new Socket(hostname, port)){
+            System.out.println("Client connected to server");
+
+            File file = new File("src/Client/DownloadedFile.txt");
+
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+
+            // Bad idea \/
+            // String fileName = dataInputStream.readUTF();
+            // long fileSize = dataInputStream.readLong();
+
+            byte[] buffer = new byte[1024];
+            int byteLength;
+
+            while ((byteLength = dataInputStream.read(buffer)) != -1){
+                fileOutputStream.write(buffer, 0, byteLength);
+            }
+
+            fileOutputStream.close();
+            dataInputStream.close();
+            socket.close();
+
+            System.out.println("File received successfully");
+
+        }catch (IOException e){
+            System.out.println("IO error:  " + e.getMessage());
+        }
+    }
 }
--- src/TcpServer.java
+++ src/TcpServer.java
@@ -1,2 +1,45 @@
+import javax.imageio.IIOException;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class TcpServer {
+    public static void main(String[] args){
+
+        try(ServerSocket serverSocket = new ServerSocket(5000)){
+
+            System.out.println("Server is listening on port 5000");
+
+            Socket socket = serverSocket.accept();
+            //socket.setSoTimeout(5000); //Timeout efter 5 sekunder.
+
+            System.out.println("New client connected");
+
+            File file = new File("src/Server/test.txt");
+            if (!file.exists()){
+                System.out.println("File does not exists");
+                return;
+            }
+
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            FileInputStream fileInputStream = new FileInputStream(file);
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = fileInputStream.read(buffer)) != -1){
+                dataOutputStream.write(buffer, 0, bytesRead);
+            }
+
+            fileInputStream.close();
+            dataOutputStream.close();
+            socket.close();
+            System.out.println("File sent successfully");
+
+
+        } catch (IOException e){
+            System.out.println("Server exception: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
